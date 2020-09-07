8ab7cd8f488f6418224ff1f3553a7d3782faa5e6
JYL827 <1345044954@qq.com>
2020-09-07 20:40:38 +0800

    vuex-demo

---
+++ vuex-demo/src/vuex/store.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  count: 1
+}
+
+const getters = {
+  abc: (state) => {
+    return state.count += 100
+  }
+}
+
+const mutations = {
+  add(state) {
+    state.count++
+  },
+  reduce(state) {
+    // console.log(abc)
+    state.count--
+  }
+}
+
+const actions = { // 提交mutations里面的方法
+  addAction(context) {
+    context.commit('add')
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  getters,
+  actions
+})
